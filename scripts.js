f6d1e4146ff0d6ed9c1d1134ac375d03f8fb9b27
WebertiBarbosa <30930934+WebertiBarbosa@users.noreply.github.com>
2020-07-02 19:33:04 -0300

    correção de erros no modal

--- scripts.js
+++ scripts.js
@@ -5,11 +5,14 @@ const cards2 = document.querySelectorAll('.card')
 for (let card of cards2) {
     card.addEventListener ('click', function (){
         const imagemId = card.getAttribute('id')
-       
+        const receita = card.querySelector('h4').innerHTML
+        const autor = card.querySelector('h3').innerHTML
 
         modalOverlay.classList.add('active')
 
         modalOverlay.querySelector('img').src = `images/${imagemId}`
+        modalOverlay.querySelector('h4').innerHTML = `${receita}`
+        modalOverlay.querySelector('h3').innerHTML = `${autor}`
        
     })
 }
